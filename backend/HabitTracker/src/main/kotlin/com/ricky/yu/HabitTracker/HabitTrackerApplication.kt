82d8387a8fad51245dd9e19e498246679c8116b9
Ricky Yu <rickyyu2294@gmail.com>
2024-11-11 21:09:37 -0800

    Init backend

---
+++ backend/HabitTracker/src/main/kotlin/com/ricky/yu/HabitTracker/HabitTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.ricky.yu.HabitTracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HabitTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HabitTrackerApplication>(*args)
+}
